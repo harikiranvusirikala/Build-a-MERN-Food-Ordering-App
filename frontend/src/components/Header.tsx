efd4e81ffc2d2d616fa900e3172901e2bbf3867b
Hari Kiran Vusirikala <vusirikalaharikiran@gmail.com>
2024-08-23 16:47:19 +0530

    Create desktop nav

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import MobilveNav from "./MobilveNav";
+import MainNav from "./MainNav";
 
 export default function Header() {
   return (
@@ -13,6 +14,9 @@ export default function Header() {
         </Link>
         <div className="md:hidden">
           <MobilveNav />
+        </div>
+        <div className="hidden md:block">
+          <MainNav />
         </div>
       </div>
     </div>
